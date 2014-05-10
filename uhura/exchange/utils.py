4aecc9be1e2e8074a20606e65db3f9e6283eb8d3
Nar Kumar <neokya@gmail.com>
2014-05-11 01:23:32 +0530

    add utils

---
+++ uhura/exchange/utils.py
@@ -0,0 +1,9 @@
+"""
+Utilities and helper functions
+"""
+
+def get_object_or_none(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
